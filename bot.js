17bb09252a12d80315511f617c26221a0d6df4cb
Duy Minh Le <dle14@slb.com>
2020-08-25 14:58:14 -0500

    add roles

--- bot.js
+++ bot.js
@@ -1,27 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
 
- 
-
-client.on('message', message => {
-
+client.on('message', (message) => {
     if (message.content === 'ping') {
+        message.reply('pong');
+        const guild = message.member.guild;
 
-       message.reply('pong');
-
-       }
-
+        guild.roles.cache.forEach((role) => {
+            message.reply(role.name);
+        });
+    }
 });
-
- 
 
 // THIS  MUST  BE  THIS  WAY
 
