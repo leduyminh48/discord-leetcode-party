ff966520f14e887590c9ef80af480287b59c0c02
Duy Minh Le <dle14@slb.com>
2020-08-25 16:05:41 -0500

    Add code

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const companyBot = require('./bots/company')
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -10,40 +11,7 @@ client.on('message', (message) => {
         message.reply('pong')
     }
 
-    if (message.content.startsWith('!company ')) {
-        const spl = message.content.split(' ');
-        const company = spl[spl.length - 1];
-
-        if (!company) {
-            return;
-        }
-        const guild = message.member.guild;
-
-        const role = guild.roles.cache.find(
-            (el) => el.name.toLowerCase() === company.toLowerCase()
-        );
-
-        if (role) {
-            message.member.roles.add(role).then(() => {
-                message.reply('Company added');
-            });
-            return;
-        }
-
-        guild.roles.create({
-            data: {
-                name: company,
-                color: 'BLUE',
-            }
-        }).then(() => {
-            return message.guild.roles.fetch();
-        }).then(roles => {
-            roles.cache.find(el => el.name = company);
-            message.member.roles.add(role).then(() => {
-                message.reply('Company added');
-            });
-        });
-    }
+    companyBot(message);
 });
 
 // THIS  MUST  BE  THIS  WAY
---
+++ bots/company.js
@@ -0,0 +1,36 @@
+module.exports = function (message) {
+    if (message.content.startsWith('!company ')) {
+        const spl = /\!company[\s]+([\s\w\W]+)/.exec(message);
+        const company = spl && spl[1];
+
+        if (!company) {
+            return;
+        }
+        const guild = message.member.guild;
+
+        const role = guild.roles.cache.find(
+            (el) => el.name.toLowerCase() === company.toLowerCase()
+        );
+
+        if (role) {
+            message.member.roles.add(role).then(() => {
+                message.reply(`'${role.name}' added as your company`);
+            });
+            return;
+        }
+
+        guild.roles.create({
+            data: {
+                name: company,
+                color: 'BLUE',
+            }
+        }).then(() => {
+            return message.guild.roles.fetch();
+        }).then(roles => {
+            const newRole = roles.cache.find(el => el.name = company);
+            return message.member.roles.add(newRole).then(() => newRole);
+        }).then(newRole => {
+            message.reply(`'${newRole.name}' added as your company`);
+        });
+    }
+}
